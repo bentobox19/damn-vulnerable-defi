b80292a1903388d9c540d0f7f937a1c48b607afd
bentobox19.eth <bentobox19@protonmail.com>
2024-10-07 23:30:55 -0300

    Cleanup

--- test/compromised/Compromised.t.sol
+++ test/compromised/Compromised.t.sol
@@ -119,11 +119,11 @@ contract CompromisedChallenge is Test {
     }
 
     function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes calldata data
-    ) external returns (bytes4) {
+        address,
+        address,
+        uint256 ,
+        bytes calldata
+    ) external pure returns (bytes4) {
         return IERC721Receiver.onERC721Received.selector;
     }
 
